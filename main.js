796b2158dac209d80246e77b902401097a1ec3d9
Francesco <fraancesco.bello@gmail.com>
2020-10-29 16:01:46 +0100

    stampa 5 numeri casuali

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+  var numeri = [];
+
+  for (var i = 0; i < 5; i++) {
+  //  numeri.push(getRndInteger)
+  var numeri_casuali =Math.floor(Math.random() * 100) + 1;
+  document.writeln(numeri_casuali);
+
+
+//  function hide(numeri_casuali) {
+  //  $ (numeri_casuali).hide();
+
+
+
+}
+setTimeout(function()  {hide(numeri_casuali)}, 1000)
+
+//var tempo = setTimeout(hide(1000))
+  //hide(numeri_casuali)
+  //function  {
+
+    //   var tempo = setTimeout( Hide(numeri_casuali), 1000);
+
+
+
+
+
+
+
+
+
+});
