feea23642b834d60a7e2882632fc2f71f95ace68
Francesco <fraancesco.bello@gmail.com>
2020-10-30 14:26:30 +0100

    end

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-
-  var numeri = [];
-
-  for (var i = 0; i < 5; i++) {
-  //  numeri.push(getRndInteger)
-  var numeri_casuali =Math.floor(Math.random() * 100) + 1;
-  document.writeln(numeri_casuali);
-
-
-
-
-
-}
 $(document).ready(function() {
 
-setTimeout(function()  {hide(numeri_casuali)}, 1000)
-
-//var tempo = setTimeout(hide(1000))
-  //hide(numeri_casuali)
-  //function  {
-
-    //   var tempo = setTimeout( Hide(numeri_casuali), 1000);
+  var numeri = [];
+  var numeri_utente = [];
+  var numeri_indovinati = [];
 
 
+  for (var i = 0; i < 5; i++) {
 
 
+    var numeri_casuali = Math.floor(Math.random() * 100) + 1;
+      numeri.push(numeri_casuali)
+      console.log(numeri);
+  }
+ $('.numero').text("numeri generati : " + numeri);
 
 
+  setTimeout(function()  {
 
+    $ (".numero").hide();
 
+}, 30000);
+
+  for (var i = 0; i < 5; i++) {
+      var numero_utente = parseInt(prompt('Inserisci un numero di quelli visualizzati prima'));
+          numeri_utente.push(numero_utente);
+          console.log(numeri_utente);
+    }
+    $('.numeri_scelti').text("numeri scelti : " + numeri_utente);
+    for (var i = 0; i < numeri.length; i++) {
+              var numero = numeri[i];
+              if(numeri_utente.includes(numero)) {
+                  numeri_indovinati.push(numero);
+              }
+        }
+  $('.risultato').text("hai indovinato : " + numeri_indovinati);
 
 });
