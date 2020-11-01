69c282b456306b3b00079d6977c27e0c88c11421
Francesco <fraancesco.bello@gmail.com>
2020-11-01 18:47:46 +0100

    end

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
   var numeri = [];
-  var numeri_utente = [];
   var numeri_indovinati = [];
+  var numeri_utente = [];
+
 
 
   for (var i = 0; i < 5; i++) {
@@ -16,23 +17,31 @@ $(document).ready(function() {
 
 
   setTimeout(function()  {
-
     $ (".numero").hide();
+}, 3000);
 
-}, 30000);
-
-  for (var i = 0; i < 5; i++) {
-      var numero_utente = parseInt(prompt('Inserisci un numero di quelli visualizzati prima'));
+  setTimeout(function() {
+    for (var i = 0; i < 5; i++) {
+       var numero_utente = parseInt(prompt('Inserisci un numero di quelli visualizzati prima'));
           numeri_utente.push(numero_utente);
           console.log(numeri_utente);
     }
-    $('.numeri_scelti').text("numeri scelti : " + numeri_utente);
+  },3500);
+
+
     for (var i = 0; i < numeri.length; i++) {
               var numero = numeri[i];
               if(numeri_utente.includes(numero)) {
                   numeri_indovinati.push(numero);
+                  console.log(numeri_indovinati);
               }
+
         }
-  $('.risultato').text("hai indovinato : " + numeri_indovinati);
+        $('.numero').text("numeri generati : " + numeri);
+        $('.numeri_scelti').text("numeri scelti : " + numeri_utente);
+        $ (".numero").removeClass('hide');
+
+
+        $('.risultato').text("hai indovinato : " + numeri_indovinati);
 
 });
